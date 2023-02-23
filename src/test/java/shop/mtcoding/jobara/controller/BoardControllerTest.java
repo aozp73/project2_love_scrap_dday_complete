95972d7af64e81098dd5756ca176ade5ce63bb78
aozp73 <aozp73@naver.com>
2023-02-23 16:50:48 +0900

    공고 목록보기 - Controller, Service 테스트 완료

---
+++ src/test/java/shop/mtcoding/jobara/controller/BoardControllerTest.java
@@ -0,0 +1,72 @@
+package shop.mtcoding.jobara.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import shop.mtcoding.jobara.model.Board;
+import shop.mtcoding.jobara.model.User;
+
+@Transactional
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+public class BoardControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper om;
+
+    private MockHttpSession mockSession;
+
+    @BeforeEach
+    public void setUp() {
+        User user = new User();
+        user.setId(1);
+        user.setUsername("ssar");
+        user.setPassword("1234");
+        user.setEmail("ssar@nate.com");
+        user.setCreatedAt(Timestamp.valueOf(LocalDateTime.now()));
+
+        mockSession = new MockHttpSession();
+        mockSession.setAttribute("principal", user);
+    }
+
+    @Test
+    public void boardList_test() throws Exception {
+        // given
+
+        // when
+        ResultActions resultActions = mvc.perform(
+                get("/board/list"));
+
+        Map<String, Object> map = resultActions.andReturn().getModelAndView().getModel();
+        List<Board> boardList = (List<Board>) map.get("boardList");
+
+        // String model = om.writeValueAsString(boardList);
+        // System.out.println("테스트 : " + model);
+
+        // then
+        resultActions.andExpect(status().isOk());
+        assertThat(boardList.get(1).getTitle()).isEqualTo("공고제목2");
+        assertThat(boardList.get(2).getTitle()).isEqualTo("공고제목3");
+    }
+}
