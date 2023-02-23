01fd5a352e06b2215b1d88e56fee428a24d4f066
kimalss22 <kimalss22@gmail.com>
2023-02-23 18:26:23 +0900

    기업 회원 로그인 Controller 구현
        기업 회원 로그인 Controller 구현
        테스트 코드 작성 및 테스트 통과 완료

--- src/main/java/shop/mtcoding/jobara/controller/CompanyController.java
+++ src/main/java/shop/mtcoding/jobara/controller/CompanyController.java
@@ -1,10 +1,26 @@
 package shop.mtcoding.jobara.controller;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import shop.mtcoding.jobara.dto.company.CompanyReq.CompanyLoginReqDto;
+import shop.mtcoding.jobara.ex.CustomException;
+import shop.mtcoding.jobara.model.Company;
+import shop.mtcoding.jobara.service.CompanyService;
+import shop.mtcoding.jobara.util.Verify;
 
 @Controller
 public class CompanyController {
+
+      @Autowired
+      private CompanyService companyService;
+
+      @Autowired
+      private HttpSession session;
 
       @GetMapping("/company/joinForm")
       public String joinForm() {
@@ -19,6 +35,15 @@ public class CompanyController {
       @GetMapping("/company/updateForm")
       public String updateForm() {
             return "company/updateForm";
+      }
+
+      @PostMapping("/company/login")
+      public String login(CompanyLoginReqDto companyLoginReqDto){
+            Verify.validateStiring(companyLoginReqDto.getUsername(), "유저네임을 입력하세요.");
+            Verify.validateStiring(companyLoginReqDto.getPassword(), "암호을 입력하세요.");
+            Company coPrincipal = companyService.getCompany(companyLoginReqDto);
+            session.setAttribute("coPrincipal", coPrincipal);
+            return "redirect:/";
       }
 
 }
---
+++ src/main/java/shop/mtcoding/jobara/util/Verify.java
@@ -0,0 +1,44 @@
+package shop.mtcoding.jobara.util;
+
+import org.springframework.http.HttpStatus;
+
+import shop.mtcoding.jobara.ex.CustomException;
+
+public class Verify {
+
+    public static void validateStiring(String target, String msg) {
+        if (target == null || target.isEmpty()) {
+            throw new CustomException(msg);
+        }
+    }
+
+    public static void validateStiring(String target, String msg, HttpStatus status) {
+        if (target == null || target.isEmpty()) {
+            throw new CustomException(msg, status);
+        }
+    }
+
+    public static void validateStiring(String target, String msg, HttpStatus status, String location) {
+        if (target == null || target.isEmpty()) {
+            throw new CustomException(msg, status, location);
+        }
+    }
+
+    public static void validateObject(Object target, String msg) {
+        if (target == null) {
+            throw new CustomException(msg);
+        }
+    }
+
+    public static void validateObject(Object target, String msg, HttpStatus status) {
+        if (target == null) {
+            throw new CustomException(msg, status);
+        }
+    }
+
+    public static void validateObject(Object target, String msg, HttpStatus status, String location) {
+        if (target == null) {
+            throw new CustomException(msg, status, location);
+        }
+    }
+}
