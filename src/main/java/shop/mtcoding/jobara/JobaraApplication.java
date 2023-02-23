5fd83b2d10607c29e3de5e3095333824dd3dce10
kimalss22 <kimalss22@gmail.com>
2023-02-23 11:10:27 +0900

    프로젝트 생성

---
+++ src/main/java/shop/mtcoding/jobara/JobaraApplication.java
@@ -0,0 +1,13 @@
+package shop.mtcoding.jobara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobaraApplication.class, args);
+	}
+
+}
