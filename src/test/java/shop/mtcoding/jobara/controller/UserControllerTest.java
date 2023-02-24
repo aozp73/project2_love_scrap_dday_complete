b9dc5c2e688932b06d80fc11cb9d45f544a0cc43
K-Minho <alsgh9979@naver.com>
2023-02-24 15:17:05 +0900

    유저 업데이트 테스트 완료

--- src/test/java/shop/mtcoding/jobara/controller/UserControllerTest.java
+++ src/test/java/shop/mtcoding/jobara/controller/UserControllerTest.java
@@ -4,13 +4,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
 import javax.servlet.http.HttpSession;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
@@ -22,6 +27,22 @@ public class UserControllerTest {
 
       @Autowired
       private MockMvc mvc;
+
+      @Autowired
+      private MockHttpSession session;
+
+      @BeforeEach
+      public void setUp() {
+            User user = new User();
+            user.setId(1);
+            user.setUsername("ssar");
+            user.setPassword("1234");
+            user.setEmail("ssar@nate.com");
+            user.setCreatedAt(Timestamp.valueOf(LocalDateTime.now()));
+
+            session = new MockHttpSession();
+            session.setAttribute("usPrincipal", user);
+      }
 
       @Test
       public void login_test() throws Exception {
@@ -48,5 +69,23 @@ public class UserControllerTest {
 
             // then
             resultActions.andExpect(status().is3xxRedirection());
+      }
+
+      @Test
+      public void update_test() throws Exception {
+            // given
+            String requestBody = "password=1234&email=ssar@nate.com&tel=01099876554&career=2";
+            String requestBody2 = "password=1234&email=&tel=01099876554&career=2"; // email null 테스트
+
+            // when
+            ResultActions resultActions = mvc.perform(post("/user/update").content(requestBody)
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE).session(session));
+            ResultActions resultActions2 = mvc.perform(post("/user/update").content(requestBody2)
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE).session(session));
+
+            // then
+            resultActions.andExpect(status().is3xxRedirection());
+            resultActions2.andExpect(status().is4xxClientError());
+
       }
 }
