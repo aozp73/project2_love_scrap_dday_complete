9a0184ac3bc7439a1435fbf156bb0a9f801d1a77
kimalss22 <kimalss22@gmail.com>
2023-02-24 16:41:13 +0900

    일반 회원 더미데이터 추가

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,4 +1,8 @@
 insert into user_tb(username, password, email, address, detail_address, tel, real_name, career, resume_title, resume_content, created_at) values('ssar', '1234', 'ssar@nate.com', '부산시 야호구 야동','11길','01023452344','김살',1,'이력서제목1','이력서내용1', now());
+insert into user_tb(username, password, email, address, detail_address, tel, real_name, career, resume_title, resume_content, created_at) values('love', '1234', 'love@nate.com', '부산시 야호구 상동','12길','01011111111','박사랑',0,'이력서제목2','이력서내용2', now());
+insert into user_tb(username, password, email, address, detail_address, tel, real_name, career, resume_title, resume_content, created_at) values('google', '1234', 'google@nate.com', '부산시 야구 김동','13길','01012331233','제갈구글',3,'이력서제목3','이력서내용3', now());
+insert into user_tb(username, password, email, address, detail_address, tel, real_name, career, resume_title, resume_content, created_at) values('naver', '1234', 'naver@nate.com', '부산시 박박구 쿠동','14길','01034566666','독고네이버',5,'이력서제목4','이력서내용4', now());
+insert into user_tb(username, password, email, address, detail_address, tel, real_name, career, resume_title, resume_content, created_at) values('noew', '1234', 'noew@nate.com', '부산시 상상구 모동','15길','01066666666','상냥이',6,'이력서제목5','이력서내용5', now());
 
 insert into company_tb(username, password, email, address, detail_address, tel, company_name, company_scale, company_numb, company_field, created_at) values('cos', '1234','cos@nate.com', '부산시 빡구 동동', '71길', '01012341234', '(주)미친회사', '대기업', 333322221111,' IT업', now());
 
