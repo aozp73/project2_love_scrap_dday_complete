50b0a2761ddaa7849e8385ae84a36b158b9fa3d6
K-Minho <alsgh9979@naver.com>
2023-03-02 18:28:58 +0900

    sha util 추가

---
+++ src/main/java/shop/mtcoding/jobara/common/util/Sha.java
@@ -0,0 +1,22 @@
+package shop.mtcoding.jobara.common.util;
+
+import java.security.MessageDigest;
+
+public class Sha {
+
+      public static String sha256(String password) throws Exception {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(password.getBytes("UTF-8"));
+            byte[] HashByte = md.digest();
+            StringBuffer hash = new StringBuffer();
+
+            for (byte b : HashByte) {
+                  String hexString = String.format("%02x", b);
+                  hash.append(hexString);
+            }
+
+            String passwordHash = hash.toString();
+            return passwordHash;
+      }
+
+}
